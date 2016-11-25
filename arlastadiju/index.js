c3998ae5cd839f880bf0a538a6a6fef491254b46
Mikael Brockman <mikael@brockman.se>
2016-11-25 18:30:11 +0200

    cleanup layout, make hover friendlier

--- arlastadiju/index.js
+++ arlastadiju/index.js
@@ -1,3 +1,6 @@
+var places = {}
+var currentPlace
+
 function array(x) { return [].slice.call(x) }
 function queryAll(x, selector) { return array(x.querySelectorAll(selector)) }
 function query(x, selector) { return x.querySelector(selector) }
@@ -17,16 +20,16 @@ function debounce (func, wait, immediate) {
   }
 }
 
-var showPopup = debounce(function (place) {
+var showPopup = function (place) {
   place.marker.openPopup()
-  map.setView(place.coords, 16)
+  // map.setView(place.coords, 16)
   // window.open(location.href + "#stasts" + (i + 1), "_blank")
-}, 100)
+}
 
 onload = function () {
   var t17Coords = [56.943148, 24.123707]
 
-  var map = L.map("mapdiv", {
+  var map = L.map("mapelement", {
     zoomControl: false,
     scrollWheelZoom: false,
     attributionControl: false
@@ -47,9 +50,6 @@ onload = function () {
 
   var aside = query(document, "aside")
 
-  var places = {}
-  var currentPlace
-
   var allMarkers = []
 
   // Collect data about places from HTML
@@ -57,13 +57,13 @@ onload = function () {
     x.id = "vieta" + (i + 1)
 
     var place = {
-      name: query(x, "h2").innerText,
+      name: query(x, "h3 .name").innerText,
       coords: query(x, "coords").innerText.split(", "),
       description: query(x, "p.summary").innerText,
       story: array(
         queryAll(x, "p:not(.summary)")
       ).map(
-        function (x) { return x.innerHTML }
+        function (x) { return x.outerHTML }
       ).join("\n")
     }
 
@@ -75,8 +75,8 @@ onload = function () {
   if (location.hash.match(/stasts(\d+)$/)) {
     var i = RegExp.$1
     currentPlace = places[i]
-    query(document, "#title").innerHTML = currentPlace.name
-    query(document, "#byline").innerHTML = "<b>Tikšanās ar Lastādiju</b>"
+    query(document, "h1").innerHTML = currentPlace.name
+    query(document, "h2").innerHTML = "Tikšanās ar Lastādiju"
     aside.innerHTML = currentPlace.story
     aside.className = "visible"
     document.body.className = "story"
@@ -86,6 +86,8 @@ onload = function () {
     aside.className = ""
     document.body.className = ""
   }
+
+  var hoverTimeout
 
   queryAll(document, "article").forEach(function (x, i) {
     var place = places[i + 1]
@@ -114,9 +116,17 @@ onload = function () {
       popup(place.coords, place.name, place.description, i + 1)
     }
 
-    x.onmouseover = function () {
-      showPopup(place)
+    x.onmouseenter = function () {
+      hoverTimeout = setTimeout(function () {
+        showPopup(place)
+      }, 500)
+      console.log("setting timeout", place.name, hoverTimeout)
       return false;
+    }
+
+    x.onmouseleave = function () {
+      console.log("clearing timeout", hoverTimeout)
+      clearTimeout(hoverTimeout)
     }
 
     x.onclick = function () {
