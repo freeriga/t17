ae4c761fe40354e723f185150461ec2f017ff340
Mikael Brockman <mikael@brockman.se>
2017-01-09 18:24:34 +0200

    update banners

--- lastadija/screenshot.sh
+++ lastadija/screenshot.sh
@@ -2,10 +2,10 @@
 set -ex
 export WEBDRIVER_BROWSER=firefox
 export WEBDRIVER_SESSION=$(wd new-session)
-for x in stasts/*; do
+for x in lv/stasts/* ru/stasts/*; do
   wd go http://localhost:3000/lastadija/$x/banner.html
   sleep 0.5
-  rm $x/*.png
+  rm -f $x/*.png
   wd screenshot > $x/banner.png
   mogrify -crop 1200x630+0+0 $x/banner.png
 done
